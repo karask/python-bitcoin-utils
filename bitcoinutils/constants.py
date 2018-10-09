bcb2158bcea526d2b1e68eb97f7ad8133ff8dc0b
karask <kkarasavvas@gmail.com>
2018-10-09 13:06:35 +0300

    added P2pkhAddress class

--- bitcoinutils/constants.py
+++ bitcoinutils/constants.py
@@ -1,5 +1,9 @@
 
-NETWORK_BASE58_WIF_PREFIXES = { 'mainnet': b'\x80',
-                                'testnet': b'\xef' }
+NETWORK_WIF_PREFIXES = { 'mainnet': b'\x80',
+                         'testnet': b'\xef' }
+
+NETWORK_P2PKH_PREFIXES = { 'mainnet': b'\x00',
+                           'testnet': b'\x6f' }
+
 #                                      {'p2sh' : b'\x05'}},
 #                                      {'p2sh' : b'\xc4'}}
--- bitcoinutils/keys.py
+++ bitcoinutils/keys.py
@@ -9,14 +9,15 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 
-from hashlib import sha256
+import re
+import hashlib
 from binascii import unhexlify, hexlify
 from base58check import b58encode, b58decode
 from ecdsa import SigningKey, VerifyingKey, SECP256k1
 from sympy.ntheory import sqrt_mod
 
 # TODELETE if any of these is updated WE NEED to uninstall/install lib again
-from bitcoinutils.constants import NETWORK_BASE58_WIF_PREFIXES
+from bitcoinutils.constants import NETWORK_WIF_PREFIXES, NETWORK_P2PKH_PREFIXES
 from bitcoinutils.setup import setup, get_network
 
 class PrivateKey:
@@ -93,13 +94,13 @@ class PrivateKey:
         checksum = data_bytes[-4:]
 
         # verify key with checksum
-        data_hash = sha256(sha256(key_bytes).digest()).digest()
+        data_hash = hashlib.sha256(hashlib.sha256(key_bytes).digest()).digest()
         if not checksum == data_hash[0:4]:
             raise ValueError('Checksum is wrong. Possible mistype?')
 
         # get network prefix and check with current setup
         network_prefix = key_bytes[:1]
-        if NETWORK_BASE58_WIF_PREFIXES[get_network()] != network_prefix:
+        if NETWORK_WIF_PREFIXES[get_network()] != network_prefix:
             raise ValueError('Using the wrong network!')
 
         # remove network prefix
@@ -124,13 +125,13 @@ class PrivateKey:
         """
 
         # add network prefix to the key
-        key_bytes = NETWORK_BASE58_WIF_PREFIXES[get_network()] + self.to_bytes()
+        key_bytes = NETWORK_WIF_PREFIXES[get_network()] + self.to_bytes()
 
         if compressed == True:
             key_bytes += b'\x01'
 
         # double hash and get the first 4 bytes for checksum
-        data_hash = sha256(sha256(key_bytes).digest()).digest()
+        data_hash = hashlib.sha256(hashlib.sha256(key_bytes).digest()).digest()
         checksum = data_hash[0:4]
 
         # suffix the key bytes with the checksum and encode to base58check
@@ -239,12 +240,13 @@ class PublicKey:
         key_hex = hexlify(self.key.to_string())
 
         if compressed:
-            # check if y is even or odd
+            # check if y is even or odd (02 even, 03 odd)
             if int(key_hex[-2:], 16) % 2 == 0:
                 key_str = b'02' + key_hex[:64]
             else:
                 key_str = b'03' + key_hex[:64]
         else:
+            # uncompressed starts with 04
             key_str = b'04' + key_hex
 
         return key_str.decode('utf-8')
@@ -252,16 +254,22 @@ class PublicKey:
     def get_address(self, compressed=True):
         """Returns the corresponding Address (default compressed)"""
 
-        return "new Addres(TODO)"
+        pubkey = unhexlify( self.to_hex(compressed) )
+        hashsha256 = hashlib.sha256(pubkey).digest()
+        hashripemd160 = hashlib.new('ripemd160')
+        hashripemd160.update(hashsha256)
+        hash160 = hashripemd160.digest()
+        addr_string_hex = hexlify(hash160).decode('utf-8')
+        return P2pkhAddress(hash160=addr_string_hex)
 
 
-class Address:
-    """Represents a Bitcoin address
+class P2pkhAddress:
+    """Represents a Bitcoin address derived from a public key
 
     Attributes
     ----------
-    address : str
-        the address string
+    hash160 : str
+        the hash160 representation of the address
 
     Methods
     -------
@@ -269,13 +277,18 @@ class Address:
         instantiates an object from address string encoding
     from_hash160(hash160_str)
         instantiates an object from a hash160 hex string
-    to_address(compressed=True)
+    to_address()
         returns the address's string encoding
-    to_hash160(compressed=True))
+    to_hash160()
         returns the address's hash160 hex string representation
+
+    Raises
+    ------
+    TypeError
+        If an invalid address or hash160 is provided.
     """
 
-    def __init__(self, address, hash160=None):
+    def __init__(self, address=None, hash160=None):
         """
         Parameters
         ----------
@@ -284,16 +297,99 @@ class Address:
         hash160 : str
             the hash160 hex string representation
         """
-        pass
+
+        if hash160:
+            if self._is_hash160_valid(hash160):
+                self.hash160 = hash160
+            else:
+                raise ValueError("Invalid value for parameter hash160.")
+        elif address:
+            if self._is_address_valid(address):
+                self.hash160 = self._address_to_hash160(address)
+            else:
+                raise ValueError("Invalid value for parameter address.")
+        else:
+            raise TypeError("A valid address or hash160 is required.")
+
+    @classmethod
+    def from_address(cls, address):
+        """blah"""
+
+        return cls(address=address)
 
 
+    @classmethod
+    def from_hash160(cls, hash160):
+        """blah"""
 
+        return cls(hash160=hash160)
+
+
+    def _address_to_hash160(self, address):
+        addr_encoded = address.encode('utf-8')
+        data_checksum = b58decode( addr_encoded )
+        network_prefix = data_checksum[:1]
+        data = data_checksum[1:-4]
+        #checksum = data_checksum[-4:]
+        return hexlify(data).decode('utf-8')
+
+
+    def _is_hash160_valid(self, hash160):
+        # check the size -- should be 20 bytes, 40 characters in hexadecimal string
+        if len(hash160) != 40:
+            return False
+
+        # check all (string) digits are hex
+        try:
+            int(hash160, 16)
+            return True
+        except ValueError:
+            return False
+
+
+    def _is_address_valid(self, address):
+
+        digits_58_pattern = r'[^123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]'
+
+        # check for invalid characters
+        if re.search(digits_58_pattern, address):
+            return False
+
+        # check for length (26-35 characters)
+        # TODO: need to confirm the possible length!
+        if len(address) < 26 or len(address) > 35:
+            return False
+
+        # check address' checksum
+        data_checksum = b58decode( address.encode('utf-8') )
+        data = data_checksum[:-4]
+        checksum = data_checksum[-4:]
+
+        data_hash = hashlib.sha256(hashlib.sha256(data).digest()).digest()
+
+        if data_hash[0:4] != checksum:
+            return False
+
+        return True
+
+    def to_hash160(self):
+        return self.hash160
+
+    def to_address(self):
+        hash160_encoded = self.hash160.encode('utf-8')
+        hash160_bytes = unhexlify(hash160_encoded)
+
+        data = NETWORK_P2PKH_PREFIXES[get_network()] + hash160_bytes
+        data_hash = hashlib.sha256(hashlib.sha256(data).digest()).digest()
+        checksum = data_hash[0:4]
+        address_bytes = b58encode( data + checksum )
+        return address_bytes.decode('utf-8')
 
 
 def main():
     setup('mainnet')
-    priv = PrivateKey(secret_exponent = 1)
-    #priv = PrivateKey.from_wif('KzVpbhbE6vF8HhybZLypQw8qgGsj53KrT7njHQNcrCiboFrVT9jY')
+    #priv = PrivateKey(secret_exponent = 1)
+    priv = PrivateKey.from_wif('KzVpbhbE6vF8HhybZLypQw8qgGsj53KrT7njHQNcrCiboFrVT9jY')
     print(priv.to_bytes())
     print(priv.to_wif())
     print(priv.to_wif(compressed=False))
@@ -303,6 +399,12 @@ def main():
     print(pub.to_bytes())
     print(pub.to_hex())
     print(pub.to_hex(compressed = False))
+    a = pub.get_address(compressed=False)
+    print("+++++++++")
+    #a = P2pkhAddress('1andreas3batLhQa2FawWjeyjCqyBzypd')
+    print(a.to_hash160())
+    a2 = P2pkhAddress(hash160='0663d2403f560f8d053a25fbea618eb470716176')
+    print(a.to_address())
     #p1 = PublicKey.from_hex('040F031CA83F3FB372BD6C2430119E0B947CF059D19CDEA98F4CEFFEF620C584F9F064F1FDE4BC07D4F48C5114680AD1ADAF5F6EAA2166F7E4B4887703A681B548')
     #print(p1.to_bytes())
     #print(p1.to_hex())
