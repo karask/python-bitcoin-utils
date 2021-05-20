571ee75cf6dbfa8c6f6969f08b873cf4a1d8bb01
dio <chatzich@gmail.com>
2021-05-20 15:36:33 +0300

    add test

--- examples/simple_tx_import_raw.py
+++ examples/simple_tx_import_raw.py
@@ -16,7 +16,7 @@ from bitcoinutils.transactions import Transaction, TxInput, TxOutput
 from bitcoinutils.keys import P2pkhAddress, PrivateKey
 from bitcoinutils.script import Script
 
-def main():
+def test_non_segwit():
     # always remember to setup the network
     setup('testnet')
 
@@ -43,19 +43,19 @@ def main():
     print("\nUnsigned transaction:",tx)
     # print raw transaction
     print("\nRaw unsigned transaction:\n" + tx.serialize())
-    tx_from_raw = Transaction.import_from_raw(tx.serialize())
+    tx_from_raw = Transaction.from_raw(tx.serialize())
     print("\nUnsigned from raw transaction:",tx_from_raw)
     print("\nUnsigned from raw transaction raw:",tx_from_raw.serialize())
 
     if tx_from_raw.serialize() == tx.serialize():
-        print("SUCCESS import_from_raw Serialization OK")
+        print("SUCCESS from_raw Serialization OK")
     else:
-        print("ERROR import_from_raw Serialization failed")
+        print("ERROR from_raw Serialization failed")
 
     if str(tx) == str(tx_from_raw):
-        print("SUCCESS import_from_raw OK")
+        print("SUCCESS from_raw OK")
     else:
-        print("ERROR import_from_raw failed")
+        print("ERROR from_raw failed")
 
     # use the private key corresponding to the address that contains the
     # UTXO we are trying to spend to sign the input
@@ -79,24 +79,102 @@ def main():
 
     print("\nSigned transaction:",tx)
     # print raw signed transaction ready to be broadcasted
-    print("\nRaw signed transaction:\n" + signed_tx)
-    tx_from_raw = Transaction.import_from_raw(tx.serialize())
+    print("\nRaw signed transaction:",signed_tx)
+    tx_from_raw = Transaction.from_raw(tx.serialize())
     print("\nSigned from raw transaction:",tx_from_raw)
     print("\nSigned from raw transaction raw:",tx_from_raw.serialize())
 
     if tx_from_raw.serialize() == tx.serialize():
-        print("SUCCESS signed import_from_raw Serialization OK")
+        print("SUCCESS signed from_raw Serialization OK")
     else:
-        print("ERROR signed import_from_raw Serialization failed")
+        print("ERROR signed from_raw Serialization failed")
 
     if str(tx) == str(tx_from_raw):
-        print("SUCCESS signed import_from_raw OK")
+        print("SUCCESS signed from_raw OK")
     else:
-        print("ERROR signed import_from_raw failed")
+        print("ERROR signed from_raw failed")
+
+def test_segwit():
+    setup('testnet')
+
+    # the key that corresponds to the P2WPKH address
+    priv = PrivateKey("cVdte9ei2xsVjmZSPtyucG43YZgNkmKTqhwiUA8M4Fc3LdPJxPmZ")
+
+    pub = priv.get_public_key()
+
+    fromAddress = pub.get_segwit_address()
+    print(fromAddress.to_string())
+
+    # amount is needed to sign the segwit input
+    fromAddressAmount = to_satoshis(0.01)
+
+    # UTXO of fromAddress
+    txid = '13d2d30eca974e8fa5da11b9608fa36905a22215e8df895e767fc903889367ff'
+    vout = 0
+
+    toAddress = P2pkhAddress('mrrKUpJnAjvQntPgz2Z4kkyr1gbtHmQv28')
+
+    # create transaction input from tx id of UTXO
+    txin = TxInput(txid, vout)
+
+    # the script code required for signing for p2wpkh is the same as p2pkh
+    script_code = Script(['OP_DUP', 'OP_HASH160', pub.to_hash160(),
+                          'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+
+    # create transaction output
+    txOut = TxOutput(to_satoshis(0.009), toAddress.to_script_pub_key())
+
+    # create transaction without change output - if at least a single input is
+    # segwit we need to set has_segwit=True
+    tx = Transaction([txin], [txOut], has_segwit=True)
+
+    print("\nUnsigned transaction:",tx)
+    # print raw transaction
+    print("\nRaw unsigned transaction:\n" + tx.serialize())
+    tx_from_raw = Transaction.from_raw(tx.serialize())
+    print("\nUnsigned from raw transaction:",tx_from_raw)
+    print("\nUnsigned from raw transaction raw:",tx_from_raw.serialize())
+
+    if tx_from_raw.serialize() == tx.serialize():
+        print("SUCCESS from_raw Serialization OK")
+    else:
+        print("ERROR from_raw Serialization failed")
+
+    if str(tx) == str(tx_from_raw):
+        print("SUCCESS from_raw OK")
+    else:
+        print("ERROR from_raw failed")
+
+
+
+
+    sig = priv.sign_segwit_input(tx, 0, script_code, fromAddressAmount)
+    tx.witnesses.append( Script([sig, pub.to_hex()]) )
+
+    # print raw signed transaction ready to be broadcasted
+    print("\nSigned transaction:",tx)
+    # print raw signed transaction ready to be broadcasted
+    print("\nRaw signed transaction:",tx)
+    tx_from_raw = Transaction.from_raw(tx.serialize())
+    print("\nSigned from raw transaction:",tx_from_raw)
+    print("\nSigned from raw transaction raw:",tx_from_raw.serialize())
+
+    if tx_from_raw.serialize() == tx.serialize():
+        print("SUCCESS signed from_raw Serialization OK")
+    else:
+        print("ERROR signed from_raw Serialization failed")
+
+    if str(tx) == str(tx_from_raw):
+        print("SUCCESS signed from_raw OK")
+    else:
+        print("ERROR signed from_raw failed")
+
+
+
 
  
 
-
 if __name__ == "__main__":
-    main()
+    test_non_segwit()
+    test_segwit()
 
