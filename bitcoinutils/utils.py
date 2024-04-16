712f0e8781f340976f03b061063c22bacd22df2c
guptamukund22 <b22cs086@iitj.ac.in>
2024-04-16 20:50:20 +0530

    Exception Thrown added

--- bitcoinutils/utils.py
+++ bitcoinutils/utils.py
@@ -125,7 +125,6 @@ def get_tag_hashed_merkle_root(
     Scripts is a list of list of Scripts describing the merkle tree of scripts to commit
     Example of scripts' list:  [ [A, B], C ]
     """
-    # TODO raise errors
 
     # empty scripts or empty list
     if not scripts:
@@ -149,8 +148,8 @@ def get_tag_hashed_merkle_root(
             right = get_tag_hashed_merkle_root(scripts[1])
             return tapbranch_tagged_hash(left, right)
         else:
-            # TODO throw exception
-            exit("List cannot have more than 2 branches.")
+            # Raise an error if a branch node contains more than two elements
+            raise ValueError("Invalid Merkle branch: List cannot have more than 2 branches.")
 
 
 def to_satoshis(num: int | float | Decimal):
