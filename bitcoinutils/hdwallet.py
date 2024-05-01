d468626fd9e22b22234580ff8440ae6054bc841d
guptamukund22 <b22cs086@iitj.ac.in>
2024-05-01 17:59:49 +0530

    from_mnemonic

--- bitcoinutils/hdwallet.py
+++ bitcoinutils/hdwallet.py
@@ -23,32 +23,49 @@ from binascii import unhexlify
 import unicodedata
 
 class HDW:
-    def __init__(self, seed):
-        seed_bytes = unhexlify(seed)  # Convert hex string to bytes
-        self.seed = seed_bytes
-        self.master_private_key, self.master_chain_code = self.from_seed(seed_bytes)
+    def __init__(self, seed: Optional[str] = None):
+        """
+        Initialize the HD Wallet with a seed if provided.
+
+        Args:
+            seed (Optional[str]): A hexadecimal string representing the seed from which the HD Wallet will derive its keys.
+        """
+        self.strength: Optional[int] = None
+        if seed:
+            seed_bytes = unhexlify(seed)  # Convert hex string to bytes
+            self.seed = seed_bytes
+            # Generate the master private key and master chain code from the seed.
+            self.master_private_key, self.master_chain_code = self.from_seed(seed_bytes)
 
     def from_seed(self, seed_bytes):
-        """ Generate the master keys from the seed bytes """
+        """
+        Generate the master keys from the seed bytes.
+
+        Args:
+            seed_bytes (bytes): Seed from which the master private key and chain code are derived.
+
+        Returns:
+            tuple: Tuple containing (master_private_key, master_chain_code).
+        """
+
         key = b"Bitcoin seed"
         h = hmac.new(key, seed_bytes, hashlib.sha512).digest()
+        # Split the hash into two halves: private key and chain code.
         master_private_key = h[:32]
         master_chain_code = h[32:]
         return master_private_key, master_chain_code
     
     @staticmethod
-    def get_mnemonic_strength(mnemonic: str, language: str = None) -> int:
+    def get_mnemonic_strength(mnemonic: str) -> int:
         """
         Get mnemonic strength.
 
         :param mnemonic: Mnemonic words.
         :type mnemonic: str
-        :param language: Mnemonic language, default to None.
-        :type language: str
 
         :returns: int -- Mnemonic strength.
         """
-
+        
         words = len(unicodedata.normalize("NFKD", mnemonic).split(" "))
         if words == 12:
             return 128
@@ -62,6 +79,45 @@ class HDW:
             return 256
         else:
             raise ValueError("Unsupported number of words in mnemonic.")
+
+    def from_mnemonic(self, mnemonic: str, passphrase: str = "") -> "HDW":
+        """
+        Create keys from a mnemonic phrase.
+
+        Args:
+            mnemonic (str): Mnemonic phrase used to generate the seed.
+            passphrase (str): Additional passphrase used with the mnemonic for added security.
+
+        Returns:
+            HDW: Returns itself after initializing the master keys.
+        """
+
+        self.strength = self.get_mnemonic_strength(mnemonic=self._mnemonic)
+        normalized_mnemonic = unicodedata.normalize("NFKD", mnemonic)
+        seed = self.to_seed(normalized_mnemonic, passphrase)
+        self.master_private_key, self.master_chain_code = self.from_seed(seed)
+        return self
+
+    def to_seed(self, mnemonic: str, passphrase: str = "") -> bytes:
+        """
+        Generate a seed from a mnemonic and a passphrase.
+
+        Args:
+            mnemonic (str): Mnemonic phrase.
+            passphrase (str): Optional passphrase for additional security.
+
+        Returns:
+            bytes: The seed generated from the mnemonic and passphrase.
+        """
+
+        passphrase = "mnemonic" + passphrase
+        mnemonic_bytes = mnemonic.encode("utf-8")
+        passphrase_bytes = passphrase.encode("utf-8")
+        #PBKDF2 HMAC-SHA512 to derive the seed from the mnemonic and passphrase.
+        stretched = hashlib.pbkdf2_hmac(
+            "sha512", mnemonic_bytes, passphrase_bytes, 2048
+        )
+        return stretched[:64]
 
 class HDWallet:
     """Wraps the python hdwallet library to provide basic HD wallet functionality
