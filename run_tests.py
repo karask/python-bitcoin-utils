34b7ea332fb9432d6213fdc212c4f9be085bde46
JAGADISHSUNILPEDNEKAR <jsphere16@gmail.com>
2025-03-20 10:54:25 +0530

    Remove non-PSBT related files from PR

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-# Create a test loader and discover tests in the 'tests' directory
-loader = unittest.TestLoader()
-suite = loader.discover('tests')
-
-# Run the tests
-runner = unittest.TextTestRunner()
-runner.run(suite)
