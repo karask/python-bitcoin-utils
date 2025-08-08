4f454d546bc06cd717971991de938f5606e450cd
Your Name <kkarasavvas@gmail.com>
2025-08-08 11:35:00 +0300

    added example that convert bitcoin node's key+derivation path to legacy and segwit for WIF, pubkey, address

---
+++ examples/convert_node_hdd_to_WIF.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2018-2025 The python-bitcoin-utils developers
+#
+# This file is part of python-bitcoin-utils
+#
+# It is subject to the license terms in the LICENSE file found in the top-level
+# directory of this distribution.
+#
+# No part of python-bitcoin-utils, including this file, may be copied,
+# modified, propagated, or distributed except according to the terms contained
+# in the LICENSE file.
+
+
+from bitcoinutils.setup import setup
+from bitcoinutils.hdwallet import HDWallet
+
+
+def main():
+    # always remember to setup the network
+    setup("testnet")
+
+    # get an HDWallet wrapper object by extended private key and path
+    xprivkey = (
+        "tprv8ZgxMBicQKsPekNkbvEF7XrgepZcUB4PpYtK3iHahuMitcp9kEPXQnXCbLr5BPc"
+        "rwe1aYerGLEMpEpgvUNZEpSq5oD3snvLCJGSfJbw4AuD"
+    )
+    path = "m/44'/1'/0'/0/0"
+
+    hdw = HDWallet(xprivkey, path)
+
+    print("Ext. private key:", xprivkey)
+    print("Derivation path:", path, " (default for legacy node addresses)")
+
+    # get a PrivateKey object used in bitcoinutils throughout
+    privkey = hdw.get_private_key()
+    print("WIF:", privkey.to_wif())
+
+    # get public key
+    pubkey = privkey.get_public_key()
+    print("Pubkey:", pubkey.to_hex())
+
+    # get legacy address
+    addr1 = pubkey.get_address()
+    print("Legacy address:", addr1.to_string())
+
+    # get segwit v0 address
+    addr2 = pubkey.get_segwit_address()
+    print("Segwit address:", addr2.to_string())
+
+    # get taproot (segwit v1) address
+    addr3 = pubkey.get_taproot_address()
+    print("Taproot address:", addr3.to_string())
+
+    new_path = "m/84'/1'/0'/0/0"
+    hdw.from_path(new_path)
+    print("\n\nNew derivation path:", new_path, " (default for segwit node addresses)")
+
+    # get a PrivateKey object used in bitcoinutils throughout
+    privkey = hdw.get_private_key()
+    print("WIF:", privkey.to_wif())
+
+    # get public key
+    pubkey = privkey.get_public_key()
+    print("Pubkey:", pubkey.to_hex())
+
+    # get legacy address
+    addr1 = pubkey.get_address()
+    print("Legacy address:", addr1.to_string())
+
+    # get segwit v0 address
+    addr2 = pubkey.get_segwit_address()
+    print("Segwit address:", addr2.to_string())
+
+    # get taproot (segwit v1) address
+    addr3 = pubkey.get_taproot_address()
+    print("Taproot address:", addr3.to_string())
+
+    print("\nFor nested segwit use path: m/49'/1'/0'/0/")
+    print("\nFor taproot use path: m/86'/1'/0'/0/")
+
+
+if __name__ == "__main__":
+    main()
